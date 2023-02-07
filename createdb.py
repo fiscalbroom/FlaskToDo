2f40b511900b48117cda4466c9fdeb4b75db84f3
Quindici <fiscalbroom@gmail.com>
2023-02-07 21:30:20 +0100

    #1

--- createdb.py
+++
@@ -1,6 +0,0 @@
-from backend import backend
-from data.provider import db
-
-with backend.app_context():
-    db.init_app(backend)
-    db.create_all()
--- run.py
+++
@@ -1,4 +0,0 @@
-from backend import backend
-
-if __name__ == 'index':
-    backend.run()
